28137a95ece5a8ed5e9063549b5a8bbc9c12203e
Amy <33735421+Sylvamy@users.noreply.github.com>
2025-12-18 14:37:54 +0000

    Update BlockHandler.java

--- src/main/java/dev/espi/protectionstones/BlockHandler.java
+++ src/main/java/dev/espi/protectionstones/BlockHandler.java
@@ -37,6 +37,7 @@ import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.Arrays;
